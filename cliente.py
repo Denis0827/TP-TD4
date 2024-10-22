00b08b6268b517ae78faa75a5fc4a87cfb2567f4
Denis0827 <163213831+Denis0827@users.noreply.github.com>
2024-10-22 03:14:07 -0300

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -1,55 +1,68 @@
 import canalruidoso as f # Correr pip install canalruidoso en la terminal
 from scapy.all import * # Correr pip install scapy en la terminal
+from scapy.layers.inet import IP,TCP
+import random
 
-def calculo_chksum(packet):
+def calcular_chksum(packet):
     packet[TCP].chksum = None
     longitud_bytes = len(bytes(packet[TCP]))
     packet[TCP].chksum = longitud_bytes
+
+def crear_paquete(puerto_origen, puerto_destino, nro_seq, nro_ack, flags_a_enviar) -> packet:
+    source_ip = '127.0.0.1'
+    dest_ip = '127.0.0.1'
+    src_port = puerto_origen
+    dest_port = puerto_destino
+
+    # Creamos la parte de IP
+    ip = IP(dst = dest_ip, src = source_ip)
+
+    # Creamos la parte de TCP
+    tcp = TCP(dport = dest_port, sport = src_port, seq = nro_seq, ack = nro_ack, flags = flags_a_enviar)
+
+    # Los combinamos
+    packet = ip/tcp
+    calcular_chksum(packet)
     
-def esperar_paquete(listen_port, timeout, mostrar) -> List:
+    return packet
+
+
+def esperar_ack(listen_port, tiempo_timeout, mostrar) -> List:
     interface = "Software Loopback Interface 1" 
     print(f"Listening for TCP packets on port {listen_port}...")
     filter_str = f"tcp port {listen_port}"
     if mostrar:
-        pkt_capturado = sniff(iface = interface, filter = filter_str, prn = lambda x: x.show(), count = 1, timeout = 3)
+        pkt_capturado = sniff(iface = interface, filter = filter_str, prn = lambda x: x.show(), count = 1, timeout = tiempo_timeout)
     else: 
-        pkt_capturado = sniff(iface = interface, filter = filter_str, count = 1, timeout = 3)
+        pkt_capturado = sniff(iface = interface, filter = filter_str, count = 1, timeout = tiempo_timeout)
     return pkt_capturado
 
-# Elegimos parametros
-source_ip = '127.0.0.1'
-dest_ip = '127.0.0.1'
-dest_port = 8000
-src_port = 5000
 
-# Creamos la parte de IP
-ip = IP(dst=dest_ip,src =source_ip)
+def enviar_paquete(flags_a_enviar, seq_ack):
+    pkt_capturado = []
+    while True: 
+        # len(pkt_capturado) == 0 or pkt_capturado[0][TCP].ack < nro_secuencia: # ver
+        pkt = crear_paquete(5000, 8000, seq_ack[0], seq_ack[1], flags_a_enviar)
+        f.envio_paquetes_inseguro(pkt)
+        print("Se ha enviado el paquete de seq " + str(pkt[TCP].seq) + " y ack " + str(pkt[TCP].ack) + ".")
+        pkt_capturado = esperar_ack(5000, 3, False)
+        if len(pkt_capturado) == 0: 
+            print("Se retransmite el paquete de seq " + str(pkt[TCP].seq) + " por timeout.")
+        elif pkt_capturado[0][TCP].chksum != len(bytes(pkt_capturado[0][TCP])):
+            print("Se retransmite el paquete de seq " + str(pkt[TCP].seq) + " por corrupción de datos.")
+        else:
+            seq_ack[1] = pkt_capturado[0][TCP].seq + 1
+            print("Se ha recibido correctamente el paquete de seq " + str(pkt_capturado[0][TCP].seq) + " y ack " + str(pkt_capturado[0][TCP].ack) + ".")
+            break
 
-# Creamos la parte de TCP
-tcp = TCP(dport=dest_port, sport =src_port)
+nro_random = random.randint(1, 10000)
+seq_y_ack = [nro_random, 0]
+enviar_paquete(['S'], seq_y_ack)
 
-# Los combinamos
-packet = ip/tcp
-calculo_chksum(packet)
+seq_y_ack[0] += 1
+enviar_paquete(['A'], seq_y_ack)
 
-# "Enviamos" el paquete
-f.envio_paquetes_inseguro(packet)
 
-pkt_capturado = []
-cant_retransmisiones = 0
-pkt_capturado = esperar_paquete(5000, 3, False)
-while(len(pkt_capturado) == 0):
-    cant_retransmisiones += 1
-    f.envio_paquetes_inseguro(packet)
-    pkt_capturado = esperar_paquete(5000, 3, False)
-    print("Cantidad de retransmisiones del SYN: " + str(cant_retransmisiones))
-
-ip = IP(dst = pkt_capturado[0][IP].src, src = pkt_capturado[0][IP].dst)
-tcp = TCP(dport = pkt_capturado[0][TCP].sport, sport = pkt_capturado[0][TCP].dport, flags = ['A'])
-ack_packet = ip/tcp
-f.envio_paquetes_inseguro(ack_packet)
-
-    
 
 
 
--- servidor.py
+++ servidor.py
@@ -1,31 +1,65 @@
 from scapy.all import *
 import canalruidoso as f # Correr pip install canalruidoso en la terminal
-from scapy.all import TCP
+from scapy.layers.inet import IP,TCP
 
 # Mostramos todas las interfaces
-print(conf.ifaces)
+# print(conf.ifaces)
 
-interface = "Software Loopback Interface 1" # Esto lo tienen que completar con el nombre de la interfaz que tenga el 127.0.0.1 si se recibe el paquete en la misma computadora que lo envio.
+def calcular_chksum(packet):
+    packet[TCP].chksum = None
+    longitud_bytes = len(bytes(packet[TCP]))
+    packet[TCP].chksum = longitud_bytes
 
-listen_port = 8000  # Elegir el puerto que esta escuchando
+def esperar_paquete(listen_port, tiempo_timeout, mostrar) -> List:
+    interface = "Software Loopback Interface 1" 
+    print(f"Listening for TCP packets on port {listen_port}...")
+    filter_str = f"tcp port {listen_port}"
+    pkt_capturado = []
+    if mostrar:
+        pkt_capturado = sniff(iface = interface, filter = filter_str, prn = lambda x: x.show(), count = 1, timeout = tiempo_timeout)
+    else: 
+        pkt_capturado = sniff(iface = interface, filter = filter_str, count = 1, timeout = tiempo_timeout)
+    return pkt_capturado
 
-print(f"Listening for TCP packets on port {listen_port}...")
-filter_str = f"tcp port {listen_port}"
-
-# Escuchar en ese puerto
-pkt_capturado = sniff(iface = interface, filter = filter_str, prn = lambda x: x.show(), count = 1, timeout = 60)
-
-pkt_corrupto = pkt_capturado[0][TCP].chksum != len(bytes(pkt_capturado[0][TCP]))
-if not(pkt_corrupto):
+def crear_respuesta(pkt_capturado, nro_seq):
     ip = IP(dst = pkt_capturado[0][IP].src, src = pkt_capturado[0][IP].dst)
-    tcp = TCP(dport = pkt_capturado[0][TCP].sport, sport = pkt_capturado[0][TCP].dport, flags = ['S', 'A'])
-    syn_ack_packet = ip/tcp
-    f.envio_paquetes_inseguro(syn_ack_packet)
-    
+    if 'S' in pkt_capturado[0][TCP].flags: 
+        tcp = TCP(dport = pkt_capturado[0][TCP].sport, sport = pkt_capturado[0][TCP].dport, seq = nro_seq, 
+            ack = pkt_capturado[0][TCP].seq + 1, flags = ['S', 'A'])
+    else:
+        tcp = TCP(dport = pkt_capturado[0][TCP].sport, sport = pkt_capturado[0][TCP].dport, seq = nro_seq, 
+                ack = pkt_capturado[0][TCP].seq + 1, flags = ['A']) 
+    packet = ip/tcp
+    calcular_chksum(packet)
+    return packet
 
-interface = "Software Loopback Interface 1" 
-listen_port = 8000  
-print(f"Listening for TCP packets on port {listen_port}...")
-filter_str = f"tcp port {listen_port}"
-pkt_capturado = sniff(iface = interface, filter = filter_str, prn = lambda x: x.show(), count = 1, timeout = 60)
+
+def enviar_respuesta(nro_secuencia):
+    pkt_capturado = esperar_paquete(8000, 60, False)
+    while True:
+        if len(pkt_capturado) != 0:
+            pkt_corrupto = pkt_capturado[0][TCP].chksum != len(bytes(pkt_capturado[0][TCP]))
+            if not(pkt_corrupto):
+                packet = crear_respuesta(pkt_capturado, nro_secuencia)
+                f.envio_paquetes_inseguro(packet)
+                print(f"Se ha enviado un paquete de secuencia {packet[TCP].seq} y de ack {packet[TCP].ack}")
+                break
+    
+nro_secuencia = random.randint(1, 10000)
+pkt_capturado = esperar_paquete(8000, 60, False)
+while True:
+    if len(pkt_capturado) != 0:
+        pkt_corrupto = pkt_capturado[0][TCP].chksum != len(bytes(pkt_capturado[0][TCP]))
+        if not(pkt_corrupto):
+            packet = crear_respuesta(pkt_capturado, nro_secuencia)
+            f.envio_paquetes_inseguro(packet)
+            print(f"Se ha enviado un paquete de secuencia {packet[TCP].seq} y de ack {packet[TCP].ack}")
+            break
+
+while True:
+    pkt_capturado = esperar_paquete(8000, 60, False)
+    if len(pkt_capturado) != 0:
+        packet = crear_respuesta(pkt_capturado, pkt_capturado[0][TCP].ack)
+        f.envio_paquetes_inseguro(packet)
+        print(f"Se ha enviado un paquete de secuencia {packet[TCP].seq} y de ack {packet[TCP].ack}")
 
