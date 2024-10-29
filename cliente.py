8f10b23c94c47bf4a34ad0a5b27eb20d391d4cb7
Denis0827 <163213831+Denis0827@users.noreply.github.com>
2024-10-29 18:55:11 -0300

    Add files via upload

--- cliente.py
+++ cliente.py
@@ -12,14 +12,9 @@ while enviar_syn:
     pkt_capturado = escuchar(5000, 3, False)
     paquete_perdido = len(pkt_capturado) == 0
     paquete_corrupto = len(pkt_capturado) != 0 and not(verificar_checksum(pkt_capturado[0]))
-    paquete_inesperado = len(pkt_capturado) != 0 and pkt_capturado[0][TCP].ack != seq_ack[0] + 1 # tengo que chequear tmb si el ack = seq y seq = ack?
     
     if paquete_perdido: 
         print("Se retransmite el paquete de seq " + str(seq_ack[0]) + " por timeout. Flags: S.")
-        print("=====")
-        enviar_request(5000, 8000, ['S'], seq_ack)
-    elif paquete_inesperado: # esta al pedo esto porque solo le puede llegar el synack
-        print("Se ha recibido un paquete distinto al apropiado. Se espera que se reconozca el paquete de seq " + str(seq_ack[0]) + " y ack " + str(seq_ack[1]) + ".")   
         print("=====")
         enviar_request(5000, 8000, ['S'], seq_ack)
     elif paquete_corrupto:
@@ -29,17 +24,17 @@ while enviar_syn:
     else:
         print("Se ha reconocido correctamente el paquete de seq " + str(seq_ack[0]) + " y ack " + str(seq_ack[1]) + ". Flags: S.")
         print("=====")
-        seq_ack[0] += 1
-        seq_ack[1] = pkt_capturado[0][TCP].seq + 1
-        enviar_respuesta(pkt_capturado, seq_ack[0])
         enviar_syn = False
 
+seq_ack[0] = pkt_capturado[0][TCP].ack
+seq_ack[1] = pkt_capturado[0][TCP].seq + 1
+enviar_respuesta(pkt_capturado, seq_ack[0])
 pkt_capturado = []
 enviar_ack = True
 
 while enviar_ack:
     pkt_capturado = escuchar(5000, 10, False)
-    ack_recibido = len(pkt_capturado) == 0   # si le llegó correctamente el ack al servidor, no debería capturar ningún otro paquete
+    ack_recibido = len(pkt_capturado) == 0   
     paquete_corrupto = len(pkt_capturado) != 0 and not(verificar_checksum(pkt_capturado[0]))
     paquete_esperado = len(pkt_capturado) != 0 and pkt_capturado[0][TCP].ack == seq_ack[0]
     
@@ -53,9 +48,10 @@ while enviar_ack:
         print("=====")
         enviar_ack = False
 
+seq_ack[0] += 1 # corregir
+seq_ack[1] += 1
 pkt_capturado = []
 esperar_fin = True
-seq_ack[0] += 1
 
 while esperar_fin:
     pkt_capturado = escuchar(5000, 30, False)
@@ -73,10 +69,9 @@ while esperar_fin:
         print("Se ha recibido un paquete distinto al apropiado. Se espera que se reconozca el paquete de seq " + str(seq_ack[0]) + " y ack " + str(seq_ack[1]) + ".")   
         print("=====")
     else:
-        seq_ack[1] += 1
-        enviar_respuesta(pkt_capturado, pkt_capturado[0][TCP].ack)
         esperar_fin = False
     
+enviar_respuesta(pkt_capturado, pkt_capturado[0][TCP].ack)
 pkt_capturado = []
 enviar_fin_ack = True
 
@@ -99,10 +94,8 @@ while enviar_fin_ack:
         print("=====")
         enviar_request(5000, 8000, ['F', 'A'], seq_ack)
     else:
-        print("Se ha reconocido correctamente el paquete de seq " + str(pkt_capturado[0][TCP].seq) + " y ack " + str(pkt_capturado[0][TCP].ack) + ". Flags: FA.")
+        print("Se ha reconocido correctamente el paquete de seq " + str(seq_ack[0]) + " y ack " + str(seq_ack[1]) + ". Flags: FA.")
         print("=====")
-        seq_ack[0] = pkt_capturado[0][TCP].ack
-        seq_ack[1] = pkt_capturado[0][TCP].seq + 1
         enviar_fin_ack = False
 
 
--- servidor.py
+++ servidor.py
@@ -10,8 +10,6 @@ while esperar_syn:
     pkt_capturado = escuchar(8000, 30, False)
     paquete_perdido = len(pkt_capturado) == 0
     paquete_corrupto = len(pkt_capturado) != 0 and not(verificar_checksum(pkt_capturado[0]))
-    #paquete_inesperado = len(pkt_capturado) != 0 and pkt_capturado[0][TCP].ack != 0 and pkt_capturado[0][TCP].seq != seq_ack[0] 
-    #nunca me puede llegar uno inesperado
 
     if paquete_perdido:
         print("No se ha capturado ningún paquete.")
@@ -20,15 +18,9 @@ while esperar_syn:
         print("El paquete recibido de seq " + str(pkt_capturado[0][TCP].seq) + " y ack " + str(pkt_capturado[0][TCP].ack) + " está corrupto. Flags: S.")
         print("=====")
     else: 
-        # Medio innecesario (preguntar). Nunca iria a pasar porque siempre la secuencia del ack te tiene que llegar en 0. 
-        if pkt_capturado[0][TCP].ack == 0:
-            seq_ack[1] = pkt_capturado[0][TCP].seq + 1
-            enviar_respuesta(pkt_capturado, seq_ack[0])
-            esperar_syn = False
-        elif pkt_capturado[0][TCP].ack != 0 and pkt_capturado[0][TCP].seq == seq_ack[1]:
-            seq_ack[1] += 1
-            enviar_respuesta(pkt_capturado, pkt_capturado[0][TCP].ack)
-            esperar_syn = False
+        seq_ack[1] = pkt_capturado[0][TCP].seq + 1
+        enviar_respuesta(pkt_capturado, seq_ack[0])
+        esperar_syn = False
 
 pkt_capturado = []
 enviar_syn_ack = True
@@ -54,11 +46,11 @@ while enviar_syn_ack:
     else:
         print("Se ha reconocido correctamente el paquete de seq " + str(pkt_capturado[0][TCP].seq) + " y ack " + str(pkt_capturado[0][TCP].ack) + ". Flags: SA.")
         print("=====")
-        seq_ack[0] += 1
-        seq_ack[1] = pkt_capturado[0][TCP].seq + 1
-        # los seqs y acks pueden aumentar siempre en 1 o dependen de lo que captura?
+        
         enviar_syn_ack = False
 
+seq_ack[0] = pkt_capturado[0][TCP].ack
+seq_ack[1] = pkt_capturado[0][TCP].seq + 1
 print("Esperando 20 segundos...")
 print("=====")
 time.sleep(20)
@@ -77,7 +69,7 @@ while enviar_fin:
         print("Se retransmite el paquete de seq " + str(seq_ack[0]) + " por timeout. Flags: F.")
         print("=====")
         enviar_request(8000, 5000, ['F'], seq_ack)
-    elif paquete_inesperado: # esta al pedo esto porque solo le puede llegar el synack
+    elif paquete_inesperado:
         print("Se ha recibido un paquete distinto al apropiado. Se espera que se reconozca el paquete de seq " + str(seq_ack[0]) + " y ack " + str(seq_ack[1]) + ".")   
         print("=====")
         enviar_request(8000, 5000, ['F'], seq_ack)
@@ -88,17 +80,18 @@ while enviar_fin:
     else:
         print("Se ha reconocido correctamente el paquete de seq " + str(seq_ack[0]) + " y ack " + str(seq_ack[1]) + ". Flags: F.")
         print("=====")
-        seq_ack[0] += 1
-        seq_ack[1] = pkt_capturado[0][TCP].seq + 1
-        enviar_respuesta(pkt_capturado, seq_ack[0])
+        
         enviar_fin = False
 
+seq_ack[0] = pkt_capturado[0][TCP].ack
+seq_ack[1] = pkt_capturado[0][TCP].seq + 1
+enviar_respuesta(pkt_capturado, seq_ack[0])
 pkt_capturado = []
 enviar_ack = True
 
 while enviar_ack:
     pkt_capturado = escuchar(8000, 10, False)
-    ack_recibido = len(pkt_capturado) == 0   # si le llegó correctamente el ack al servidor, no debería capturar ningún otro paquete
+    ack_recibido = len(pkt_capturado) == 0  
     paquete_corrupto = len(pkt_capturado) != 0 and not(verificar_checksum(pkt_capturado[0]))
     fin_ack_retransmitido = len(pkt_capturado) != 0 and pkt_capturado[0][TCP].ack == seq_ack[0] 
     
